df8a34d0035de73d38ace3d464e64ed1e6439ed7
veeoid <vc7410@rit.edu>
2025-10-22 21:05:30 -0500

    added local storage

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-const BASE_URL = 'http://api.weatherapi.com/v1';
+const BASE_URL = 'https://api.weatherapi.com/v1';
 const API_KEY = '80953d4ac6244516b9e183506252210'
 
 const cityInput = document.getElementById('city')
 const submitBtn = document.getElementById('submit-btn');
 const output = document.getElementById('output');
+const loginBtn = document.getElementById('login');
 
+
+const loginForm = document.getElementById('login-form');
+const username = document.getElementById('username');
+const password = document.getElementById('password');
 
 async function get_weather(city){
     try{
@@ -14,19 +19,15 @@ async function get_weather(city){
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
         const data = await response.json();
-        console.log(data)
         return `The temperature in ${data.location.name} is ${data.current.temp_c}°C`;
     }
     catch (error){
         console.log(error);
         return `Sorry, could not get weather for ${city}.`;
-    }
-
-}
+    }}
 
 const weather = submitBtn.addEventListener('click', async (event)=>{
     event.preventDefault();
-
     const city = cityInput.value;
 
     if (!city){
@@ -36,6 +37,30 @@ const weather = submitBtn.addEventListener('click', async (event)=>{
 
     output.textContent = 'Fetching Data.....'
     const weatherText = await get_weather(city);
-    
     output.textContent = weatherText;
+});
+
+
+loginBtn.addEventListener('click', () => {
+  $('#login-modal').modal('show');
+});
+
+
+loginForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const user = username.value.trim();
+  const pass = password.value;
+
+  console.log('login modal open triggered');
+
+
+  if (!user || !pass) {
+    alert('Please enter username and password');
+    return;
+  }
+
+  localStorage.setItem('isLoggedIn', 'true');
+  localStorage.setItem('username', user);
+
+  $('#login-modal').modal('hide');
 });
